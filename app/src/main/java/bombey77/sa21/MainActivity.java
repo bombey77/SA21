3d6015ad7a38cceee5ebfcee65835c7a35a65a57
Ромашка <bombey77@mail.ru>
2016-09-26 20:37:34 +0300

    Initial commit

---
+++ app/src/main/java/bombey77/sa21/MainActivity.java
@@ -0,0 +1,41 @@
+package bombey77.sa21;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btn1, btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn1:
+                Intent intent = new Intent(this, SecondActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn2:
+                Intent intent1 = new Intent(this, ThirdActivity.class);
+                startActivity(intent1);
+                break;
+            default:
+                break;
+        }
+
+
+    }
+}
